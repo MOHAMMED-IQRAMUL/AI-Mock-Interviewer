b1644a7c789d8dcc40cf590fc6115182f725033a
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-06-19 10:26:46 +0530

    Setup gh-pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
@@ -5,6 +6,7 @@ import Practice from './pages/Practice';
 import Resources from './pages/Resources';
 import Interviews from './pages/Interviews';
 import NotFound from './pages/NotFound';
+
 import DSA from './pages/resources/DSA';
 import Core from './pages/resources/Core';
 import HR from './pages/resources/HR';
@@ -13,21 +15,23 @@ import Sheets from './pages/resources/Sheets';
 import Projects from './pages/resources/Projects';
 import Webdev from './pages/resources/Webdev';
 import Resume from './pages/resources/Resume';
+
 import InterviewSession from './pages/interview/InterviewSession';
 import SheetDetails from './pages/practive/SheetDetails';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/AI-Mock-Interviewer">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/practice" element={<Practice />} />
-        <Route path="/interviews" element={<Interviews />} />
-        <Route path="/resources" element={<Resources />} />
-        
-          <Route path="/practice/:sheetId" element={<SheetDetails />} />
+        <Route path="/practice/:sheetId" element={<SheetDetails />} />
 
+        <Route path="/interviews" element={<Interviews />} />
+        <Route path="/interview/start" element={<InterviewSession />} />
+
+        <Route path="/resources" element={<Resources />} />
         <Route path="/resources/dsa" element={<DSA />} />
         <Route path="/resources/core" element={<Core />} />
         <Route path="/resources/hr" element={<HR />} />
@@ -37,12 +41,8 @@ export default function App() {
         <Route path="/resources/webdev" element={<Webdev />} />
         <Route path="/resources/resume" element={<Resume />} />
 
-
-        <Route path="/interview/start" element={<InterviewSession />} />
-        
         {/* Catch-all route for 404 */}
         <Route path="*" element={<NotFound />} />
-
       </Routes>
     </Router>
   );
